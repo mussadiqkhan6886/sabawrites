fee8aa5084966d54cdaa55f407013f3ffca7d99d
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-23 17:14:23 +0500

    starting with layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { quicksand } from "@/lib/fonts/font";
 
 
 export const metadata: Metadata = {
@@ -15,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`${quicksand.className} antialiased`}
       >
         {children}
       </body>
