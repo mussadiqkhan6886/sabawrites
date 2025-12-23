20e0e45f5cb6e88d6246b7bdf76d7eb06ef10568
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-23 18:17:03 +0500

    animted svg on navigation now styling header

--- components/UserComp/Navigation.tsx
+++ components/UserComp/Navigation.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link'
 import React from 'react'
 import { FiInstagram, FiFacebook, FiX } from "react-icons/fi";
 import { FaTiktok, FaPinterest } from "react-icons/fa";
-import {motion} from "framer-motion"
+import {delay, motion} from "framer-motion"
+import Image from 'next/image';
 
 const Navigation = ({close}: {close: () => void}) => {
 
@@ -38,7 +39,21 @@ const Navigation = ({close}: {close: () => void}) => {
             }
         },
     }
-
+    const variant2 = {
+        hidden: {
+            opacity: 0,
+            x: -420
+        },
+        view: {
+            opacity: 1,
+            x:0,
+            transition: {
+                duration: 0.4,
+                ease: "easeOut",
+                delay: 0.3
+            }
+        },
+    }
 
   return (
     <motion.nav variants={variant} initial={"hidden"} animate={"view"} exit={"exit"} className='w-[320px] sm:w-[400px] h-screen flex flex-col justify-between p-10 border-r border-dark bg-medium fixed top-0 z-50'>
@@ -48,6 +63,9 @@ const Navigation = ({close}: {close: () => void}) => {
             <li onClick={close} className='text-lg tracking-wider hover:text-dark hover:scale-110 transition hover:shadow-2xl py-1 hover:font-semibold' key={item.title}><Link href={item.link}>{item.title}</Link></li>
         ))}
       </ul>
+      <motion.div className='absolute -right-18 bottom-30' variants={variant2} initial={"hidden"} animate={"view"}>
+        <Image src={"/spin.svg"} alt='flower image animation' width={150} height={150} className='spinAnimation ' />
+      </motion.div>
       <div className='flex justify-between items-center sm:px-5'>
         <Link onClick={close} href="#"><FiInstagram className="text-2xl text-dark hover:text-main" /></Link>
         <Link onClick={close} href="#"><FaTiktok className="text-2xl text-dark hover:text-main" /></Link>
