05db5a53cdcca880dd148dde6dd118dcc616831e
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2025-12-28 13:45:52 +0500

    done

--- components/UserComp/SectionTitle.tsx
+++ components/UserComp/SectionTitle.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SectionTitle = ({firstWord, secondWord}: {firstWord: string, secondWord: string}) => {
   return (
-    <h3 className={`${dancing.className} text-3xl sm:text-5xl mb-10 font-semibold text-center `}>
+    <h3 className={`${dancing.className} text-4xl sm:text-5xl mb-10 font-semibold text-center `}>
       <span>{firstWord}</span><span className={`inline-block ml-8`}> {secondWord}</span>
     </h3>
   )
