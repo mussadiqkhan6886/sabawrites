759402c17d15f904f97b8d93e5559aadde212773
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-01-03 03:01:12 +0500

    fixed bug

--- components/UserComp/Blogs.tsx
+++ components/UserComp/Blogs.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import SectionTitle from './SectionTitle'
 import BlogsList from './BlogsList'
-import { blogs } from '@/lib/constants'
-import Image from 'next/image'
 import { connectDB } from '@/lib/database'
 import BlogSchema from '@/lib/schema/BlogSchema'
 
@@ -22,13 +20,6 @@ const Blogs = async () => {
       <div>
         <BlogsList data={data} />
       </div>
-        {/* <Image
-        width={1000}
-        height={1000}
-          src="/flower-3.png"
-          alt="flower iamge"
-          className="absolute -right-20 bottom-0 w-full scale-x-[-1]"
-        /> */}
     </section>
   )
 }
