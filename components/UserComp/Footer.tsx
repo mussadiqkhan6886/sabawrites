767b9b9b169da01f059fd469182ea2fcf8525bec
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-01-04 22:24:59 +0500

    fixed footer added links

--- components/UserComp/Footer.tsx
+++ components/UserComp/Footer.tsx
@@ -18,25 +18,31 @@ export default function Footer() {
   
   return (
     <footer className="text-gray-800 bg-white  mt-12 relative">
-      <div className="absolute inset-0 w-full h-full bg-white/40" />
-      <div className="max-w-7xl z-40 mx-auto px-6 py-12 grid grid-cols-1 sm:grid-cols-2  gap-8">
-        {/* Brand */}
+      <div className="max-w-7xl z-40 mx-auto px-6 py-12 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+
         <div className="space-y-4 z-30 relative">
-          <h5 className={`${playfair.className} text-[27px] sm:text-4xl`}>Saba <span className='italic font-semibold'>Writes</span></h5>
-          {/* <Image src={"/spin.svg"} alt="spin image footer" width={50} height={50} className="spinAnimation absolute -top-4 left-30 md:right-17" /> */}
+          <h6 className={`${playfair.className} text-[27px] sm:text-4xl`}>Saba <span className='italic font-semibold'>Writes</span></h6>
           <p className="text-black">
             A lifestyle & beauty blog celebrating elegance, wellness, and feminine energy.
           </p>
         </div>
 
-          <div className="flex flex-col space-y-3 mt-2 z-30">
-            <h6 className="text-lg font-semibold mb-4">Social Links</h6>
-            <div className="flex gap-6">
-              <Link className="text-2xl" aria-label="instagram link" href={"#"}><FaInstagram name="instagram" aria-label="instagram" /></Link>
-              <Link className="text-2xl" aria-label="facebook link" href={"#"}><FaFacebook name="facebook" aria-label="facebook" /></Link>
-              <Link className="text-2xl" aria-label="pinterest link" href={"#"}><FaPinterest name="pinterest" aria-label="pinterest" /></Link>
-            </div>
+        <div className="flex flex-col space-y-3 mt-2 z-30">
+          <h6 className="text-lg font-semibold mb-4">Social Links</h6>
+          <div className="flex gap-6">
+            <Link className="text-2xl" aria-label="instagram link" href={"#"}><FaInstagram name="instagram" aria-label="instagram" /></Link>
+            <Link className="text-2xl" aria-label="facebook link" href={"#"}><FaFacebook name="facebook" aria-label="facebook" /></Link>
+            <Link className="text-2xl" aria-label="pinterest link" href={"#"}><FaPinterest name="pinterest" aria-label="pinterest" /></Link>
           </div>
+        </div>
+
+        <div>
+          <h6>Quick Links</h6>
+          <Link href={"/privacy-policy"}>Privacy Policy</Link>
+          <Link href={"/terms-and-condition"}>Terms And Condition</Link>
+          <Link href={"/disclaimer"}>Disclaimer</Link>
+          <Link href={"/cookie-policy"}>Cookies Policy</Link>
+        </div>
       </div>
 
       <div className="border-t border-gray-300 mt-8 py-4 text-center text-black text-sm">
